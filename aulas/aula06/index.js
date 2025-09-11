19d8a13c3003df6b1163e1e12c85e8e7b5f06f1e
Arthurr214 <arthurbarrossilva325@gmail.com>
2025-09-11 10:45:10 -0300

    codigos da aula 06 e 07

--- aulas/aula06/index.js
+++ aulas/aula06/index.js
@@ -9,6 +9,22 @@ app.get("/", (req,res) => {
     res.send("Ola");
 })
 
+// middleware de aplicação
+app.use((req, res, next ) => {
+    console.log("Passei pelo middleware de app");
+    next();
+})
+
+
+
+app.use('/tarefas', router);
+
+
+// middleware de erro
+app.use((err, req, res, next) => {
+   res.status(500).send(err.message);
+});
+
 // 3. iniciar a aplicação em uma porta
 app.listen(3000, ()=>{
     console.log("app esta on");
